e126bdad47668132c358c1793b1d5c42cd43db47
Brian Olsen <brian@maven-group.org>
2014-05-22 00:39:43 +0200

    Added tests of supplying a DATABASE_URL to the test action.

--- tests/provider/test_test.sh
+++ tests/provider/test_test.sh
@@ -24,6 +24,21 @@ testAdminLink() {
     docker run --rm --link $NAME:dokdb $PROVIDER test -q --admin
 }
 
+testURL() {
+  make_container NON_LINKED
+  local ip="$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' $NON_LINKED)"
+  local url="$(docker run --rm --link $NON_LINKED:dokdb $PROVIDER url)"
+  sleep 6
+
+  assertSucceeds "Test connection" \
+    docker run --rm -e "DATABASE_URL=$url" --link $NAME:blabla $PROVIDER test -q
+}
+
+testURLTakesPrecedent() {
+  assertFails "Test connection" \
+    docker run --rm -e "DATABASE_URL=test://127.0.0.1/testname" --link $NAME:dokdb $PROVIDER test -q
+}
+
 testRestart() {
   make_container RESTARTED
   sleep 6
